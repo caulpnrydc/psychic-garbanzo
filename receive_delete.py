1f4d9a0d3b9d066a0216bd5e1460cb2bdbcea062
CaulpnryDC <kris.bunney@gmail.com>
2020-11-10 14:31:48 -0500

    added tracing into receive_delete.py

--- receive_delete.py
+++ receive_delete.py
@@ -1,7 +1,68 @@
 import boto3
 import argparse
+from jaeger_client import Config
+import logging
+import time
 
 
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.WARNING)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    return config.initialize_tracer()
+
+tracer = init_tracer('sqs-empty')
+
+
+# Receive messages from sqs, write to json file, upload to s3
+def receive_message(sqs_queue):
+    with tracer.start_span('receive-message') as span:
+        sqs = boto3.client('sqs')
+
+        span.set_tag('sqs_queue', queue_url)
+        span.log_kv({'event': 'string-format', 'value': queue_url})
+
+        response = sqs.receive_message(
+                QueueUrl=queue_url,
+                AttributeNames=[
+                    'SentTimestamp'
+                    ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                    ],
+                VisibilityTimeout=0,
+                WaitTimeSeconds=0
+                )
+
+        message = response['Messages'][0]
+        receipt_handle = message['ReceiptHandle']
+        file.write('%s' % message + '\n')
+        file.close()
+        span.log_kv({'event': 'string-format', 'file': backup_name})
+
+        s3up = boto3.resource('s3')
+        s3up.Object(bucket,backup_name).upload_file(backup_name)
+        span.log_kv({'event': 'string-format', 'bucket': bucket})
+# Delete received messages from sqs queue
+
+        span.log_kv({'event': 'string-format', 'value': 'Delete'})
+        span.log_kv({'event': 'string-format', 'queue': queue_url})
+        sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=receipt_handle
+                )
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-q", "--queue", help="URL for SQS Queue")
@@ -15,37 +76,8 @@ backup_name = args.out
 bucket = args.bucket
 
 file = open(backup_name, 'a')
-sqs = boto3.client('sqs')
 
+receive_message(queue_url)
 
-# Receive messages from sqs, write to json file, upload to s3
-
-response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'SentTimestamp'
-            ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-            ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-        )
-
-message = response['Messages'][0]
-receipt_handle = message['ReceiptHandle']
-file.write('%s' % message + '\n')
-file.close()
-
-s3up = boto3.resource('s3')
-s3up.Object(bucket,backup_name).upload_file(backup_name)
-
-
-# Delete received messages from sqs queue
-
-sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-        )
-
+time.sleep(2)
+tracer.close()
